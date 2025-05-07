3867018310ca966c74af210f4137588046607992
Evenus11 <ewen.shackel@gmail.com>
2025-05-07 19:40:58 +0100

    inital setup of vim

---
+++ lua/eshackel/init.lua
@@ -0,0 +1,12 @@
+
+--startup test to confirm second init.lua is caled in
+print('second hello')
+
+--list of required configs
+--keybindings
+require('eshackel.remap')
+
+-- seting packer up for every launch
+require('eshackel.packer')
+
+
